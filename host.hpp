b00874d46d8692146db7891815b102f50787ab20
Lucas Dário <lucasdario.dariolucas@gmail.com>
2015-11-17 21:48:05 -0200

    Resolvendo tretas

--- host.hpp
+++ host.hpp
@@ -1,13 +1,13 @@
 #include <string>
 
 class Host {
-  private:
   public:
   	Host(std::string name) : name_(name) {}
   	void set_ip(std::string ip);
   	void set_gateway_ip(std::string ip);
   	void set_dns_server_ip(std::string ip);
-
+  
+  private:
   	std::string name_;
   	std::string ip_;
   	std::string gateway_ip_;
