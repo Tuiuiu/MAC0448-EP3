0411285d1e8529dace090b9de891ec3bc78c9a1f
Tuiuiu <lucasdario.dariolucas@gmail.com>
2015-11-22 01:38:12 -0200

    Adcionando getters

--- datagram.hpp
+++ datagram.hpp
@@ -6,9 +6,12 @@ class Datagram {
   	Datagram(const std::string &src_ip, const std::string &dst_ip, const std::string &content) 
   	: source_ip_(src_ip), destination_ip_(dst_ip), content_(content) {}
     void print_test() { std::cout << "|____ ip_src: " << source_ip_ << " dst_src: " << destination_ip_ << " content: " << content_ << std::endl; }
+    std::string get_source_ip() { return source_ip_; }
+    std::string get_destination_ip() { return destination_ip_; }
+    std::string get_content() { return content_; }
   private:	
-	  std::string source_ip_;
-	  std::string destination_ip_;
+	std::string source_ip_;
+	std::string destination_ip_;
     std::string content_;
 };
 
